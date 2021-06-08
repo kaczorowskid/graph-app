f280b5ae6e13beaa3c46caf43e2fc8a6300d8cd9
sijab <mar.jablonski96@wp.pl>
2021-06-08 13:50:47 +0200

    fix Startup.ts

--- src/utils/Startup.ts
+++ src/utils/Startup.ts
@@ -1,11 +1,11 @@
-import art from 'ascii-art';
+import art, { Art } from 'ascii-art';
 
 export default class Startup {
     constructor(readonly font: string) { }
 
     public async printStartupScreen(text: string) {
         try {
-            let rendered = await art.font(text, this.font);
+            const rendered: Art = await art.font(text, this.font);
             console.log(rendered);
         } catch (err) {
             console.log(err)
